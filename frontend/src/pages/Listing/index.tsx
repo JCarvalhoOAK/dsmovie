95b887b1a9a68608754afaff6353ea8cac6af103
Jurandi Carvalho <jurandi.carvalho@outlook.com>
2022-03-11 10:33:14 -0300

    UseState, UseEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,18 +2,25 @@ import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "components/requests";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
+   
+    const [pageNumber, setPageNumber] = useState(0);
 
-        // FORMA ERRADA
-        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
         .then(response => {
-            console.log(response.data);
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
         });
-
+    }, []);
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
@@ -38,6 +45,8 @@ function Listing() {
             </div>
         </>
     );
+
+
 }
 
 export default Listing;
